23240845b7314207c734546df277750044c979cb
Thejus Rajendran <thejusr1999@gmail.com>
2022-09-13 10:55:10 +0530

    updated the splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import {motion} from 'framer-motion'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Building ⚠️</h1>
+    <div className="flex justify-center items-center width-screen h-screen">
+      <motion.div whileHover={{scale: 1.3}}>
+          <h1 className="text-xl">⚠️ on maintenance ⚠️</h1>
+      </motion.div>
     </div>
   )
 }
