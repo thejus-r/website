e7f3295655a4f5115b11751c38606718701223bc
Thejus Rajendran <thejusr1999@gmail.com>
2022-10-17 00:41:48 +0530

    banner fixes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,9 @@
 import Banner from "../components/Banner";
 
 export default function HomePage() {
-  return <Banner />;
+  return (
+    <div className="view-port">
+      <Banner />;
+    </div>
+  );
 }
