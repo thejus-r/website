041dc5a79b3c42756de1417530147572460689db
Thejus Rajendran <thejusr1999@gmail.com>
2022-10-17 00:39:11 +0530

    this thing hacks the cursor

---
+++ src/utils/Cursor.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+export default function Cursor() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+    },
+    text: {
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: "white",
+      mixBlendMode: "difference",
+    },
+  };
+
+  return (
+    <motion.div
+      className="cursor z-50"
+      variants={variants}
+      animate="default"
+    ></motion.div>
+  );
+}
