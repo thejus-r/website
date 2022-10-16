40ada7c2d5e8dc3765448382a06e6a7ffeedd66a
Thejus Rajendran <thejusr1999@gmail.com>
2022-10-16 11:31:04 +0530

    fixed #3

--- src/utils/MenuButton.jsx
+++ src/utils/MenuButton.jsx
@@ -3,21 +3,24 @@ import { motion, useCycle } from "framer-motion";
 const iconVarients = {
   initial: { rotate: 0 },
 
-  openTop: { x: -3, y: 5, rotate: 45 },
-  openBottom: { x: -3, y: -5, rotate: -45 },
+  openTop: { x: -5, y: 5, rotate: 45 },
+  openBottom: { x: -5, y: -5, rotate: -45 },
 };
 
 export default function MenuButton({ toggleOpen, isOpen }) {
   return (
-    <div className="flex flex-col justify-center" onClick={() => toggleOpen()}>
+    <div
+      className="flex flex-col justify-center p-4"
+      onClick={() => toggleOpen()}
+    >
       <motion.div
-        className="w-[64px] h-1 bg-black m-1"
+        className="w-[48px] h-[2px] bg-black m-1"
         variants={iconVarients}
         initial="initial"
         animate={isOpen ? "openTop" : "initial"}
       ></motion.div>
       <motion.div
-        className="w-[64px] h-1 bg-black m-1"
+        className="w-[48px] h-[2px] bg-black m-1"
         variants={iconVarients}
         initial="initial"
         animate={isOpen ? "openBottom" : "initial"}
