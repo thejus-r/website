fee5e31f4d29431d191e1aa6a49767b9d9157f94
Thejus Rajendran <thejusr1999@gmail.com>
2022-10-17 20:15:20 +0530

    added state to change appearance

---
+++ src/components/NavBar/MenuButton.jsx
@@ -0,0 +1,54 @@
+import { motion } from "framer-motion";
+
+const iconVarients = {
+  initial: {
+    rotate: 0,
+    backgroundColor: "#fff",
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+    },
+  },
+
+  openTop: {
+    x: -5,
+    y: 5,
+    backgroundColor: "#000",
+    rotate: 45,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+    },
+  },
+  openBottom: {
+    x: -5,
+    y: -5,
+    backgroundColor: "#000",
+    rotate: -45,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+    },
+  },
+};
+
+export default function MenuButton({ toggleOpen, isOpen }) {
+  return (
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+      className="flex flex-col justify-center p-4"
+      onClick={() => toggleOpen()}
+    >
+      <motion.div
+        className="w-[48px] h-[2px] bg-black m-1"
+        variants={iconVarients}
+        initial="initial"
+        animate={isOpen ? "openTop" : "initial"}
+      ></motion.div>
+      <motion.div
+        className="w-[48px] h-[2px] bg-black m-1"
+        variants={iconVarients}
+        initial="initial"
+        animate={isOpen ? "openBottom" : "initial"}
+      ></motion.div>
+    </motion.div>
+  );
+}
