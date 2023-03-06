44ed9842eeb9cb9793a2c4fd565343bd0784c39f
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-06 17:56:37 +0530

    pnpm lock?

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -1,24 +1,24 @@
 import type { Project } from "../types/ProjectType";
 
 const projects: Project[] = [
-    {
-        title: "Scout",
-        type: "UI/UX",
-        desc: "A small case-study on how we can implement a modern experience for easier food exploration based on users prefererance and dieatary restrictions",
-        imgPath: "/images/scout/banner.png",
-        slug: "https://medium.com/@thejusr/scout-app-case-study-9b992d9385b4",
-        dateCreated: "2023-01-12",
-        public: true,
-    },
-    {
-        title: "CECify Radio",
-        type: "Branding",
-        desc: "A college talk show for which a calming and professional design system was created for embedding its brand identity to its motives",
-        imgPath: "/images/scout/banner.png",
-        slug: "https://medium.com/@thejusr/scout-app-case-study-9b992d9385b4",
-        dateCreated: "2022-08-19",
-        public: false,
-    },
-]
+  {
+    title: "Scout",
+    type: "UI/UX",
+    desc: "A small case-study on how we can implement a modern experience for easier food exploration based on users prefererance and dieatary restrictions",
+    imgPath: "/images/scout/banner.png",
+    slug: "https://medium.com/@thejusr/scout-app-case-study-9b992d9385b4",
+    dateCreated: "2023-01-12",
+    public: true,
+  },
+  {
+    title: "Tic-Tac-Toe",
+    type: "ReactJS Dev",
+    desc: "A simple tic-tac-toe web game build with ReactJS and TailwindCSS, while exploring libraries such as Zustand, framer-motion, etc",
+    imgPath: "/images/scout/banner.png",
+    slug: "https://medium.com/@thejusr/scout-app-case-study-9b992d9385b4",
+    dateCreated: "2023-03-06",
+    public: true,
+  },
+];
 
 export default projects;
