e8d1d07c79c86871b674d78eb9396b67ea857326
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-06 18:00:40 +0530

    update to tagline

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,44 +3,55 @@ import { motion, Variants, MotionConfig } from "framer-motion";
 const EASE = [0.32, 0.72, 0, 1];
 
 const parentVarients: Variants = {
-    "hidden": {
-        opacity: 0,
-        y: 50
+  hidden: {
+    opacity: 0,
+    y: 50,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+      when: "beforeChildren",
     },
-    "visible": {
-        y: 0,
-        opacity: 1,
-        transition: {
-            staggerChildren: 0.3,
-            when: "beforeChildren"
-        }
-    }
-}
+  },
+};
 const childrenVarients: Variants = {
-    "hidden": {
-        opacity: 0,
-        y: 50
-    },
-    "visible": {
-        y: 0,
-        opacity: 1
-    }
-}
-
-
+  hidden: {
+    opacity: 0,
+    y: 50,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 const Banner = () => {
-    return <MotionConfig transition={{ ease: EASE, duration: 1.2 }}>
-        <motion.section variants={parentVarients} initial='hidden' animate="visible" className='md:h-72 md:px-12 flex flex-col justify-end space-y-8'>
-            <motion.div variants={childrenVarients} className='text-xl md:text-4xl font-black md:max-w-2xl'>
-                <h1>Thejus is an aspirant user experience designer, seeking for full-time position right now.</h1>
-            </motion.div>
-            <motion.div variants={childrenVarients} >
-                <p className="text-base font-bold text-stone-400">Currently in</p>
-                <p className="text-base font-bold text-stone-900">Kollam, Kerala</p>
-            </motion.div>
-        </motion.section>
+  return (
+    <MotionConfig transition={{ ease: EASE, duration: 1.2 }}>
+      <motion.section
+        variants={parentVarients}
+        initial="hidden"
+        animate="visible"
+        className="md:h-72 md:px-12 flex flex-col justify-end space-y-8"
+      >
+        <motion.div
+          variants={childrenVarients}
+          className="text-xl md:text-4xl font-black md:max-w-2xl"
+        >
+          <h1>
+            Thejus is an aspirant Front-end Engineer, seeking for full-time
+            position right now.
+          </h1>
+        </motion.div>
+        <motion.div variants={childrenVarients}>
+          <p className="text-base font-bold text-stone-400">Currently in</p>
+          <p className="text-base font-bold text-stone-900">Kollam, Kerala</p>
+        </motion.div>
+      </motion.section>
     </MotionConfig>
-}
+  );
+};
 
 export default Banner;
