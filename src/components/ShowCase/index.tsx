ef2e6d788614a773d53aca721b19b6bca2c954b4
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-06 19:25:45 +0530

    refined animations and layout

--- src/components/ShowCase/index.tsx
+++ src/components/ShowCase/index.tsx
@@ -1,13 +1,53 @@
 import { Project } from "@/lib/types/ProjectType";
 import ShowCaseCard from "./ShowCaseCard";
+import { motion, Variants } from "framer-motion";
+
+const parentVariants: Variants = {
+  hidden: {},
+  show: {
+    transition: {
+      delayChildren: 1.2,
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const childrenVarients: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 50,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      ease: [0.32, 0.72, 0, 1],
+      duration: 3,
+    },
+    y: 0,
+  },
+};
 
 function ShowCase({ projects }: { projects: Project[] }) {
-    return <section className="mt-12 md:p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
-        {projects.map((project: Project, index: number) => {
-            if (index == 0) return <div key={index} className="md:col-span-2"><ShowCaseCard key={index} {...project} /></div>
-            return <ShowCaseCard key={index} {...project} />
-        })}
-    </section>
-};
+  return (
+    <motion.section
+      variants={parentVariants}
+      initial="hidden"
+      animate="show"
+      className="mt-12 md:p-6 grid grid-cols-1 md:grid-cols-2 gap-4"
+    >
+      {projects.map((project: Project, index: number) => {
+        return (
+          <motion.div
+            variants={childrenVarients}
+            key={index}
+            className={`${index === 0 ? "md:col-span-2 p-4" : "p-4"}`}
+          >
+            <ShowCaseCard {...project} />
+          </motion.div>
+        );
+      })}
+    </motion.section>
+  );
+}
 
 export default ShowCase;
