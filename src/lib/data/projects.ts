18cadc4db375dca989ff78663f591c0d42c6468a
Thejus Rajendran <thejusr1999@gmail.com>
2023-03-14 00:21:47 +0530

    added 3rd project

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -12,11 +12,20 @@ const projects: Project[] = [
   },
   {
     title: "Tic-Tac-Toe",
-    type: "ReactJS Dev",
+    type: "Web App",
     desc: "A simple tic-tac-toe web game build with ReactJS and TailwindCSS, while exploring libraries such as Zustand, framer-motion, etc",
     imgPath: "/images/tic-tac-toe/banner.png",
-    slug: "https://tic-tac-toe-steel-phi.vercel.app/",
+    slug: "https://github.com/thejus-r/tic-tac-toe",
     dateCreated: "2023-03-06",
+    public: true,
+  },
+  {
+    title: "Tasky",
+    type: "Web App",
+    desc: "A modern looking React app that helps to manage and schedule your tasks, using React Query, Redux ToolKit, TailwindCSS, Supabase and Zustand",
+    imgPath: "/images/tasky/banner.png",
+    slug: "https://github.com/thejus-r/tasky",
+    dateCreated: "2023-03-14",
     public: true,
   },
 ];
