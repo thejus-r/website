118418d5423933cece930e0d8209bcc3911b311e
Thejus Rajendran <thejusr1999@icloud.com>
2025-03-28 16:36:31 +0530

    feat: blog layout and blog card

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,13 @@
-import { defineCollection, z } from "astro:content"
+import { defineCollection, z } from "astro:content";
 
-import { glob } from "astro/loaders"
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
   schema: z.object({
-    title: z.string()
-  })
-})
+    title: z.string(),
+    description: z.string(),
+  }),
+});
 
-export const collections = { blog }
+export const collections = { blog };
