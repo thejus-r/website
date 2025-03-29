a5c17325f94e71ebef52398f315610ebb8fed5a2
Thejus Rajendran <thejusr1999@icloud.com>
2025-03-29 13:35:10 +0530

    bug: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,14 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
 
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [alpinejs()],
-  output: "server",
-  adapter: cloudflare({
-    imageService: "compile",
-  }),
+  adapter: vercel(),
 });
