86c9fd350385e7107381c7e3cceb449bf95a220b
Thejus Rajendran <thejusr1999@icloud.com>
2025-03-29 18:55:28 +0530

    fix: path resolution

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,23 @@ import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
 import cloudflare from '@astrojs/cloudflare';
 
+import path from "path"
+
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-    base: './'
+    base: "",
+    resolve: {
+      alias: {
+        "@layouts" : path.resolve(path.dirname(""), "./src/layouts"),
+        "@components" : path.resolve(path.dirname(""), "./src/components"),
+        "@styles" : path.resolve(path.dirname(""), "./src/styles"),
+        "@assets" : path.resolve(path.dirname(""), "./src/assets"),
+        "@config" : path.resolve(path.dirname(""), "./src/config"),
+      }
+    }
   },
   integrations: [alpinejs()],
   adapter: cloudflare({
