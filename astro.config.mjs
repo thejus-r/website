3ad7718aa5ce201209cfa5b40be83ddf13cedb4c
Thejus Rajendran <thejusr1999@icloud.com>
2025-03-29 13:49:21 +0530

    bug: path resolution for vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
+import path from "path";
 
 import vercel from "@astrojs/vercel";
 
@@ -9,6 +10,16 @@ import vercel from "@astrojs/vercel";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, "./src/components"),
+        '@assets': path.resolve(__dirname, "./src/assets"),
+        '@layouts': path.resolve(__dirname, "./src/layouts"),
+        '@config': path.resolve(__dirname, "./src/config"),
+        '@types': path.resolve(__dirname, "./src/types"),
+        '@styles': path.resolve(__dirname, "./src/styles"),
+      }
+    }
   },
 
   integrations: [alpinejs()],
