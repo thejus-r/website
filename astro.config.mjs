b75be68765d771e0a4735355d195fbb398d1d6dd
Thejus Rajendran <thejusr1999@icloud.com>
2025-03-29 12:39:53 +0530

    chore: removed vercel analytics and added cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import alpinejs from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import alpinejs from "@astrojs/alpinejs";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  integrations: [alpinejs()]
+  integrations: [alpinejs()],
+  adapter: cloudflare(),
 });
