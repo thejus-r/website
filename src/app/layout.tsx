c0315bed410b2d9b167e1a484a62290c5ae70848
Thejus Rajendran <thejusr1999@icloud.com>
2025-05-08 17:04:11 +0530

    fix: og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   title: "Thejus Rajendran",
   description: "Product & Experience Designer",
   openGraph: {
-    images: "/opengraph-image.png",
+    images: "/og-image.png",
   },
 };
 
