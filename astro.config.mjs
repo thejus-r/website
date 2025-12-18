a9bedac6a1cca145dc6452059aa1fc96f8d906e5
Thejus Rajendran <thejusr1999@icloud.com>
2025-12-18 20:25:16 +0530

    cloudflare wrangler configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
+
   integrations: [mdx(), sitemap()],
+
   markdown: {
     shikiConfig: {
-      theme: "kanagawa-dragon"
-    }
-  }
+      theme: "kanagawa-dragon",
+    },
+  },
+
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
+
